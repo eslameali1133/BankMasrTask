83daba672bbd52c87a3d87869cfef28a5b358ca0
eslamaliDbs <81156529+eslamaliDbs@users.noreply.github.com>
2024-09-19 17:13:30 +0300

    Initial Commit

---
+++ BankMasrTask/BankMasrTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankMasrTaskApp.swift
+//  BankMasrTask
+//
+//  Created by Eslam ALi on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BankMasrTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankMasrTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BankMasrTask
+//
+//  Created by Eslam ALi on 19/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
